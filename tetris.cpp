d1db20aaddd6a2ad7459c3b31dee6037c07afbb8
BLINK_ONCE <mkmkqoa@gmail.com>
2020-11-28 20:46:36 +0900

    tetris

---
+++ tetris.cpp
@@ -0,0 +1,373 @@
+#include <stdio.h>
+#include <windows.h>//console window
+#include <conio.h>//console and port I/O
+#include <time.h> 
+
+clock_t startDropT, endT, startGroundT;
+int x = 8, y = 0;
+RECT blockSize;
+int blockForm;
+int blockRotation = 0;
+int key;
+
+int block[7][4][4][4] = {//blocks dot
+	{ // tBlock
+		{
+			{0,0,0,0},
+			{0,1,0,0},
+			{1,1,1,0},
+			{0,0,0,0}
+		},
+		{
+			{0,0,0,0},
+			{0,1,0,0},
+			{0,1,1,0},
+			{0,1,0,0}
+		},
+		{
+			{0,0,0,0},
+			{0,0,0,0},
+			{1,1,1,0},
+			{0,1,0,0}
+		},
+		{
+			{0,0,0,0},
+			{0,1,0,0},
+			{1,1,0,0},
+			{0,1,0,0}
+		}
+	},
+	{    //lightningBlock
+		{
+			{0,0,0,0},
+			{0,1,1,0},
+			{1,1,0,0},
+			{0,0,0,0}
+		},
+		{
+			{0,0,0,0},
+			{1,0,0,0},
+			{1,1,0,0},
+			{0,1,0,0}
+		},
+		{
+			{0,0,0,0},
+			{0,1,1,0},
+			{1,1,0,0},
+			{0,0,0,0}
+		},
+		{
+			{0,0,0,0},
+			{1,0,0,0},
+			{1,1,0,0},
+			{0,1,0,0}
+		}
+	},
+	{   //lightningBlockReverse
+		{
+			{0,0,0,0},
+			{1,1,0,0},
+			{0,1,1,0},
+			{0,0,0,0}
+		},
+		{
+			{0,0,0,0},
+			{0,1,0,0},
+			{1,1,0,0},
+			{1,0,0,0}
+		},
+		{
+			{0,0,0,0},
+			{1,1,0,0},
+			{0,1,1,0},
+			{0,0,0,0}
+		},
+		{
+			{0,0,0,0},
+			{0,1,0,0},
+			{1,1,0,0},
+			{1,0,0,0}
+		}
+	},
+	{   // 1shapeBlock
+		{
+			{0,1,0,0},
+			{0,1,0,0},
+			{0,1,0,0},
+			{0,1,0,0}
+		},
+		{
+			{0,0,0,0},
+			{0,0,0,0},
+			{1,1,1,1},
+			{0,0,0,0}
+		},
+		{
+			{0,1,0,0},
+			{0,1,0,0},
+			{0,1,0,0},
+			{0,1,0,0}
+		},
+		{
+			{0,0,0,0},
+			{0,0,0,0},
+			{1,1,1,1},
+			{0,0,0,0}
+		}
+	},
+	{   // LshapeBlock
+		{
+			{0,0,0,0},
+			{1,0,0,0},
+			{1,1,1,0},
+			{0,0,0,0}
+		},
+		{
+			{0,0,0,0},
+			{1,1,0,0},
+			{1,0,0,0},
+			{1,0,0,0}
+		},
+		{
+			{0,0,0,0},
+			{1,1,1,0},
+			{0,0,1,0},
+			{0,0,0,0}
+		},
+		{
+			{0,1,0,0},
+			{0,1,0,0},
+			{1,1,0,0},
+			{0,0,0,0}
+		}
+	},
+	{   // LshapeBlockReverse
+		{
+			{0,0,0,0},
+			{0,0,1,0},
+			{1,1,1,0},
+			{0,0,0,0}
+		},
+		{
+			{1,0,0,0},
+			{1,0,0,0},
+			{1,1,0,0},
+			{0,0,0,0}
+		},
+		{
+			{0,0,0,0},
+			{1,1,1,0},
+			{1,0,0,0},
+			{0,0,0,0}
+		},
+		{
+			{0,0,0,0},
+			{1,1,0,0},
+			{0,1,0,0},
+			{0,1,0,0}
+		}
+	},
+	{   //spaureShapeBlock
+		{
+			{0,0,0,0},
+			{0,1,1,0},
+			{0,1,1,0},
+			{0,0,0,0}
+		},
+		{
+			{0,0,0,0},
+			{0,1,1,0},
+			{0,1,1,0},
+			{0,0,0,0}
+		},
+		{
+			{0,0,0,0},
+			{0,1,1,0},
+			{0,1,1,0},
+			{0,0,0,0}
+		},
+		{
+			{0,0,0,0},
+			{0,1,1,0},
+			{0,1,1,0},
+			{0,0,0,0}
+		}
+	}
+};
+
+int space[15 + 1][10 + 2] = {  //Vertical 15+1(Floor)dot, Horizontal 10+2(Wall)dot  
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,0,0,0,0,0,0,0,0,0,0,1},
+	{1,1,1,1,1,1,1,1,1,1,1,1}
+};
+
+void Init();
+void gotoxy(int x, int y);
+void CreateRandomForm();
+bool CheckCrash(int x, int y);
+void DropBlock();
+void BlockToGround();
+void RemoveLine();
+void DrawMap();
+void DrawBlock();
+void InputKey();
+
+int main() {
+	Init();
+	startDropT = clock();
+	CreateRandomForm();
+
+	while (true) {
+		DrawMap();
+		DrawBlock();
+		DropBlock();
+		BlockToGround();
+		RemoveLine();
+		InputKey();
+	}
+	return 0;
+}
+
+void Init() {//to stop cursor blinking
+	CONSOLE_CURSOR_INFO cursorInfo;
+	cursorInfo.bVisible = 0;
+	cursorInfo.dwSize = 1;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+	srand(time(NULL));
+}
+void gotoxy(int x, int y) {//cursor moving
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+void CreateRandomForm() {//random block from 7blocks
+	blockForm = rand() % 7;
+}
+bool CheckCrash(int x, int y) {//check block has crashed
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (block[blockForm][blockRotation][i][j] == 1) {
+				int t = space[i + y][j + x / 2];
+				if (t == 1 || t == 2) { //When it met wall, blocks
+					return true;
+				}
+			}
+		}
+	}
+	return false;
+}
+void DropBlock() {//block falls
+	endT = clock();
+	if ((float)(endT - startDropT) >= 800) {
+		if (CheckCrash(x, y + 1) == true) return;
+		y++;
+		startDropT = clock();
+		startGroundT = clock();
+		system("cls");
+	}
+}
+void BlockToGround() {
+	if (CheckCrash(x, y + 1) == true) {//when block crashed
+		if ((float)(endT - startGroundT) > 1500) {//check movement in 1.5s
+			//save blocks
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					if (block[blockForm][blockRotation][i][j] == 1) {
+						space[i + y][j + x / 2] = 2;
+					}
+				}
+			}
+			x = 8;
+			y = 0;
+			CreateRandomForm();
+		}
+	}
+}
+void RemoveLine() {
+	for (int i = 15; i >= 0; i--) { //without wall
+		int cnt = 0;
+		for (int j = 1; j < 11; j++) {
+			if (space[i][j] == 2) {
+				cnt++;
+			}
+		}
+		if (cnt >= 10) { //when full of blocks
+			for (int j = 0; i - j >= 0; j++) {//blocks fall when 1line removed
+				for (int x = 1; x < 11; x++) {//wall
+					if (i - j - 1 >= 0)//when it isnt ceiling
+						space[i - j][x] = space[i - j - 1][x];
+					else      // ceiling
+						space[i - j][x] = 0;
+				}
+			}
+		}
+	}
+}
+void DrawMap() {
+	gotoxy(0, 0);
+	for (int i = 0; i < 16; i++) {
+		for (int j = 0; j < 12; j++) {
+			if (space[i][j] == 1) {
+				gotoxy(j * 2, i);
+				printf("□");
+			}
+			else if (space[i][j] == 2) {
+				gotoxy(j * 2, i);
+				printf("■");
+			}
+		}
+	}
+}
+void DrawBlock() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (block[blockForm][blockRotation][i][j] == 1) {
+				gotoxy(x + j * 2, y + i);
+				printf("■");
+			}
+		}
+	}
+}
+void InputKey() {//kbhit- is there input, getch- output from buffer
+	if (_kbhit()) {
+		key = _getch();
+		switch (key) {
+		case 32: // space
+			blockRotation++;
+			if (blockRotation >= 4) blockRotation = 0;
+			startGroundT = clock();
+			break;
+		case 75: // left
+			if (CheckCrash(x - 2, y) == false) {
+				x -= 2;
+				startGroundT = clock();
+			}
+			break;
+		case 77: // right
+			if (CheckCrash(x + 2, y) == false) {
+				x += 2;
+				startGroundT = clock();
+			}
+			break;
+		case 80: // down
+			if (CheckCrash(x, y + 1) == false)
+				y++;
+			break;
+		}
+		system("cls");
+	}
+}
